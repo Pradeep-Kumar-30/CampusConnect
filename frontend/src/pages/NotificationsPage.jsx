fa09bcd2ed7c62d84d6129a285d3189e267e033e
Pradeep-Kumar-30 <pradeepkumariiitbhagalpur@gmail.com>
2026-03-17 22:30:54 +0530

    updated code

--- frontend/src/pages/NotificationsPage.jsx
+++ frontend/src/pages/NotificationsPage.jsx
@@ -26,7 +26,7 @@ export default function NotificationsPage() {
         });
 
         return () => {
-          socket.off("newNotification");
+          socket.off("newNotification") ;
         };
       }
     }
